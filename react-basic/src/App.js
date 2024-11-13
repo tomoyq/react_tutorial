a89d91ce7e30cd7a9bd0b9576286464f22ef0b40
tomoya <arata5.18830@gmail.com>
2024-11-13 18:17:49 +0900

    firstcommit

---
+++ react-basic/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Article from './components/Article';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <Article
+          title={'React入門'}
+          content={'初めてcomponentをつくりました。'}
+        />
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
