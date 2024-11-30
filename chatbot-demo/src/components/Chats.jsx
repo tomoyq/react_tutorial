3570f656a9773fdcf8cb1a12b5432efe84ce60cb
tomoya <arata5.18830@gmail.com>
2024-11-30 19:58:20 +0900

    muiのカスタマイズ

--- chatbot-demo/src/components/Chats.jsx
+++ chatbot-demo/src/components/Chats.jsx
@@ -1,9 +1,20 @@
+import { makeStyles } from '@mui/styles';
 import List from '@mui/material/List';
 import {Chat} from './index';
 
+const useStyles = makeStyles(() => ({
+    'chats': {
+      height: 380,
+      padding: 0,
+      overflow: 'auto'
+    },
+  }));
+
 const Chats = (props) => {
+    const classes = useStyles();
+
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <List className={classes.chats}>
             {props.chats.map((chat, index) => {
                 return <Chat text={chat.text} type={chat.type} key={index.toString()} />
             })}
